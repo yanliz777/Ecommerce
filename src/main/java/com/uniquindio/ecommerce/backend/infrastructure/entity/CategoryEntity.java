d81aa8f8c72e828c7d36fd16ee20476956d28972
ramaYan <yan_cristiano@hotmail.com>
2024-11-29 15:10:21 -0500

    Se crea la clase CategoryEntity en el paquete entity del paquete infrastructure. Tambén se le da solución a un error que tenia ya que la clase UserMapper no podia leer UserEntity ya que esta última lo movi de paquete al ingresarla al paquete Entity

--- src/main/java/com/uniquindio/ecommerce/backend/infrastructure/entity/CategoryEntity.java
+++ src/main/java/com/uniquindio/ecommerce/backend/infrastructure/entity/CategoryEntity.java
@@ -1,11 +1,8 @@
-/*package com.uniquindio.ecommerce.backend.infrastructure.entity;
+package com.uniquindio.ecommerce.backend.infrastructure.entity;
 
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import java.time.LocalDateTime;
 
 @Entity//Con esto le decimos a spring que esta clase tiene que mapearse a la BD
@@ -13,10 +10,11 @@ import java.time.LocalDateTime;
 @Data//Para generar los getter y setter de esta clase
 @NoArgsConstructor//Para generar constructor vacio
 public class CategoryEntity {
-    @
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//para que me lo genere de forma incremental
     private Integer id;
 
     private String name;
     private LocalDateTime dateCreated;//para guardar la fecha y hora de creación de la categoria
     private LocalDateTime dateUpdated;//Para cuando se actualiza unacategoria
-}*/
+}
