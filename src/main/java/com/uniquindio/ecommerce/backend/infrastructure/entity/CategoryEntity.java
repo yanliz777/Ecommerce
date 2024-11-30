3077f17f9021d697a4a0b9167210cdf729fe7dc4
ramaYan <yan_cristiano@hotmail.com>
2024-11-29 22:59:50 -0500

    Se modifico el retorno del controlador de Categorias para que retorne ResponseEntity para que cuando el cliente haga una request vea una respuesta más especifica segun sea esta

--- src/main/java/com/uniquindio/ecommerce/backend/infrastructure/entity/CategoryEntity.java
+++ src/main/java/com/uniquindio/ecommerce/backend/infrastructure/entity/CategoryEntity.java
@@ -3,6 +3,9 @@ package com.uniquindio.ecommerce.backend.infrastructure.entity;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import java.time.LocalDateTime;
 
 @Entity//Con esto le decimos a spring que esta clase tiene que mapearse a la BD
@@ -15,6 +18,9 @@ public class CategoryEntity {
     private Integer id;
 
     private String name;
-    private LocalDateTime dateCreated;//para guardar la fecha y hora de creación de la categoria
-    private LocalDateTime dateUpdated;//Para cuando se actualiza unacategoria
+
+    @CreationTimestamp //Para que guarde la fecha y hora automaticamente cuando se crea un usuario
+    private LocalDateTime dateCreated;//para guardar la fecha y hora de creación del usuario
+    @UpdateTimestamp//Para que actualice la fecha y hora automaticamente cuando se crea un usuario
+    private LocalDateTime dateUpdated;//Para cuando se actualiza un usuario
 }
--- src/main/java/com/uniquindio/ecommerce/backend/infrastructure/rest/CategoryController.java
+++ src/main/java/com/uniquindio/ecommerce/backend/infrastructure/rest/CategoryController.java
@@ -3,12 +3,16 @@ package com.uniquindio.ecommerce.backend.infrastructure.rest;
 import com.uniquindio.ecommerce.backend.application.CategoryService;
 import com.uniquindio.ecommerce.backend.domain.model.Category;
 import com.uniquindio.ecommerce.backend.domain.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 //http://localhost:8085
-@RequestMapping("/api/v1/categories")
+@RequestMapping("/api/v1/admin/categories")
 //http://localhost:8085/api/v1/users: para acceder a este controlador
+@Slf4j
 public class CategoryController {
 
     //inyectamos por constructor nuestro servicio CategoryService:
@@ -19,22 +23,40 @@ public class CategoryController {
     }
 
     /*
-   Con @RequestBody User user, indicamos el Body que envia el cliente en la petición.
-   Y lo que hace @RequestBody es convertir un archivo Json a un Objeto java
-   */
+   Con @RequestBody Category category, indicamos el Body/cuerpo que envia el cliente en la petición.
+   Y lo que hace @RequestBody es convertir un archivo Json a un Objeto java.
+
+    ResponseEntity<> proporciona un control completo sobre la respuesta HTTP que se envía
+    al cliente, permitiendo incluir tanto el cuerpo de los datos como un estado HTTP adecuado.
+
+    El metodo devuelve una respuesta HTTP con:
+    El objeto Category guardado en el cuerpo de la respuesta "categoryService.save(category)".
+    Un estado HTTP de 201 Created, que informa al cliente que la operación
+    fue exitosa y que un nuevo recurso ha sido creado.
+     */
     @PostMapping("/saveCategory")
-    public Category save(@RequestBody Category category) {
-        return categoryService.save(category);
+    public ResponseEntity<Category> saveCategory(@RequestBody Category category){
+        return new ResponseEntity<>(categoryService.save(category), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/findAllCategories")
+    public ResponseEntity<Iterable<Category>> findAllCategories(){
+        return new ResponseEntity(categoryService.findAll(), HttpStatus.OK);
     }
 
     /*
-    Con @PathVariable le  indicamos al metodo que cuando
-    nos envien una variable mediante la URL se convierta al
-    tipo de dato que tengo en los parametros(Integer)
-     */
+   Con @PathVariable le  indicamos al metodo que cuando
+   nos envien una variable mediante la URL se convierta al
+   tipo de dato que tengo en los parametros(Integer)
+    */
     @GetMapping("/findById/{id}")
-    public Category findById(@PathVariable Integer id ) {
-        return categoryService.findById(id);
+    public ResponseEntity<Category> findById(@PathVariable Integer id){
+        return ResponseEntity.ok(categoryService.findById(id));
     }
 
+
+    @DeleteMapping("/deleteCategoryById/{id}")
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable Integer id){
+        return ResponseEntity.ok().build();
+    }
 }
